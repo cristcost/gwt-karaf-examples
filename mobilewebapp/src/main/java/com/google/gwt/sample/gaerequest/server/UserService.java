8b1cde3cf50ed6cc92f06baed7bfb4a425cec4d6
cristcost <cristiano.costantini@gmail.com>
2013-11-15 22:58:51 +0100

    started working on mobilewebapp

---
+++ mobilewebapp/src/main/java/com/google/gwt/sample/gaerequest/server/UserService.java
@@ -0,0 +1,15 @@
+package com.google.gwt.sample.gaerequest.server;
+
+public class UserService {
+
+  public boolean isUserLoggedIn() {
+    // TODO Auto-generated method stub
+    return false;
+  }
+
+  public String createLoginURL(String redirectUrl) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+}
